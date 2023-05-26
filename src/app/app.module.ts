e641a775ee475aee2c73f92f03fb53f18dcff290
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-05-26 15:30:31 -0300

    Inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {CalculadoraModule} from './calculadora';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CalculadoraModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
